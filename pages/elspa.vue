e244ba653f8eaba32608d92b4bed8b32918f92c9
kosugi <temparamental@hotmail.com>
2022-01-15 20:32:51 +0900

    element-plus layout added.

---
+++ pages/elspa.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-container style="height: 900px; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="['1', '3']">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="1-1">Option 1</el-menu-item>
+            <el-menu-item index="1-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="1-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">Option4</template>
+            <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-menu"></i>Navigator Two</template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="2-1">Option 1</el-menu-item>
+            <el-menu-item index="2-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="2-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">Option 4</template>
+            <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-setting"></i>Navigator Three</template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="3-1">Option 1</el-menu-item>
+            <el-menu-item index="3-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="3-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">Option 4</template>
+            <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <div>
+          <NuxtLink to="/">Home page</NuxtLink>
+          <NuxtLink to="/about">About</NuxtLink>
+          <NuxtLink to="/elementpluspractice">ElementPlus</NuxtLink>
+          <NuxtLink to="/elspa">ElSpa</NuxtLink>
+        </div>
+      </el-header>
+      <el-main>
+        <el-button v-text="label" type="primary" :onclick="showalert" />
+        <el-input v-model="value" placeholder="Please input" />
+      </el-main>
+      <el-footer>
+
+      </el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+export default {
+  name: "elspa",
+  data : function () {
+    return {
+      label : "hogehoge",
+      value : "hage~~~~~"
+    }
+  },
+  methods : {
+    showalert() {
+      alert("foobar");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    <NuxtLink to="/">Home page</NuxtLink>
-    <NuxtLink to="/about">About</NuxtLink>
-    <NuxtLink to="/elementpluspractice">ElementPlus</NuxtLink>
-  </div>
+  <el-container style="height: 900px; border: 1px solid #eee">
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <div>
+          <NuxtLink to="/">Home page</NuxtLink>
+          <NuxtLink to="/about">About</NuxtLink>
+          <NuxtLink to="/elementpluspractice">ElementPlus</NuxtLink>
+          <NuxtLink to="/elspa">ElSpa</NuxtLink>
+        </div>
+      </el-header>
+      <el-main>
+
+      </el-main>
+      <el-footer>
+
+      </el-footer>
+    </el-container>
+  </el-container>
 </template>
 <script>
 
